8a75567143d48551934d98f33480181eee5faa59
Jason Rönnbom <jaydotcontact@gmail.com>
2024-03-13 11:23:09 +0100

    Created a Array

---
+++ src/main/java/com/test/ArrayDemo.java
@@ -0,0 +1,19 @@
+package com.test;
+
+public class ArrayDemo {
+
+
+    public static void main(String[] args) {
+
+        int[] numbers = {11, 23, 39};
+
+        System.out.println("Expected Output: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.print(numbers[i] + " ");
+        }
+    }
+
+
+}
+
+// Testing area
